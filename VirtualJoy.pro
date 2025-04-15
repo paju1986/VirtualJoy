bcb0bc11a42d731234003641507f627016a11dba
paju1986 <paju1986@gmail.com>
2025-04-15 22:45:39 +0200

    added first launch configuration copy

--- VirtualJoy.pro
+++ VirtualJoy.pro
@@ -1,7 +1,7 @@
 QT += quick gui
 CONFIG += c++17
 
-TARGET = virtualjoy
+TARGET = VirtualJoy
 TEMPLATE = app
 
 SOURCES += \
--- main.cpp
+++ main.cpp
@@ -2,12 +2,41 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QQuickWindow>
+#include <QStandardPaths>
+#include <QFile>
+#include <QDir>
+#include <QFileInfo>
+#include <QDebug>
+
 #include "virtualjoystick.h"
+
+
+void ensureConfigFileExists() {
+    QString configFileName = "VirtualJoy.conf";
+
+    // Where we want to store the user configuration
+    QDir dir(QStandardPaths::writableLocation(QStandardPaths::ConfigLocation));
+    QString configPath = dir.absolutePath();
+
+    qDebug() << "Config path:" << configPath;
+    QDir().mkpath(configPath); // Ensure the directory exists
+    QString fullConfigFilePath = configPath + "/VirtualJoy/" + configFileName;
+
+    // Path to default config file inside your application folder
+    QString defaultConfigPath = QCoreApplication::applicationDirPath() + "/" + configFileName;
+
+    if (!QFile::exists(fullConfigFilePath)) {
+        QFile::copy(defaultConfigPath, fullConfigFilePath);
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
     QCoreApplication::setOrganizationName("VirtualJoy");
+
+    ensureConfigFileExists();
+
     QCoreApplication::setApplicationName("VirtualJoy");
 
     QQmlApplicationEngine engine;
